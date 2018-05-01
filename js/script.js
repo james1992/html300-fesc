bc4c445b990dbc725d456fa1a74c88bd004e5917
Jay Dahlstrom <jamesd26@uw.edu>
2018-04-30 23:00:29 -0700

    Added CSS for overlay menu

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('linked')
+function menuOn() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function menuOff() {
+    document.getElementById("overlay").style.display = "none";
+}
